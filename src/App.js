d8904ff9137053cf18d454b527ae1dde5b226c55
alejovelez10 <alejovelez10@gmail.com>
2017-06-21 14:25:42 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+     constructor(){
+      super();
+
+      this.state = {
+        title: 0
+      }
+
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.title}</span>
+        <button  onClick={this.changeTitle.bind(this)} id="inc">Incrementa</button>
       </div>
     );
   }
+
+  changeTitle(){
+   this.setState(
+   {  title: this.state.title + 1
+
+
+ })
+ }
 }
 
 export default App;
